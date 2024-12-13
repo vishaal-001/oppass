014ca89ed1e8a03a4f3232c135cdf66d868ef8b4
vishaal-001 <vishaldoud7024@gmail.com>
2024-12-13 21:01:22 +0530

    commit

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,10 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+
+
+  base: '/Passop/', // Replace 'repository-name' with your GitHub repo name
+
+
   plugins: [react()],
 })
